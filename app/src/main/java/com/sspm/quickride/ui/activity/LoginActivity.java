7b68c0a499ed0fce0fc836489ca46fed83b166b0
Siddhesh Shirodkar <sssidshirodkar@gmail.com>
2017-04-17 00:46:06 +0530

    some changes

--- app/src/main/java/com/sspm/quickride/ui/activity/LoginActivity.java
+++ app/src/main/java/com/sspm/quickride/ui/activity/LoginActivity.java
@@ -1,13 +1,8 @@
 package com.sspm.quickride.ui.activity;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -19,7 +14,6 @@ import com.digits.sdk.android.DigitsSession;
 import com.digits.sdk.android.SessionListener;
 import com.sspm.quickride.R;
 import com.sspm.quickride.pojo.SharedPreference;
-import com.sspm.quickride.pojo.User;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterCore;
 
@@ -29,8 +23,8 @@ public class LoginActivity extends AppCompatActivity {
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "dezTGFPENT8TDvGAny7H9rRFS";
     private static final String TWITTER_SECRET = "6CHehmHGTC5avcjmQK5KRf6XUeC0thrZhiM1qAjPlxdPXWa7ZM";
-    public static TelephonyManager telephonyManager;
-    private static String IMEI;
+//    public static TelephonyManager telephonyManager;
+//    private static String IMEI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,13 +46,13 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void success(DigitsSession session, String phoneNumber) {
                 // TODO: associate the session userID with your user model
-                User user = new User(IMEI, phoneNumber);
+//                User user = new User(IMEI, phoneNumber);
                 /**
                  * sets phone number in sharedPreferences
                  */
                 SharedPreference.getPreference().setMyPhoneNumber(phoneNumber);
                 Intent home = new Intent(LoginActivity.this, MapsActivity.class);
-                home.putExtra("user", user);
+//                home.putExtra("user", user);
                 startActivity(home);
                 //Toast.makeText(getApplicationContext(), "Authentication successful for " + phoneNumber, Toast.LENGTH_LONG).show();
             }
@@ -71,53 +65,53 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+////        fire.becameInActive();
+//        super.onDestroy();
+//    }
 
-    @Override
-    protected void onDestroy() {
-//        fire.becameInActive();
-        super.onDestroy();
-    }
+//    @Override
+//    public void onPause() {
+//        super.onPause();  // Always call the superclass method first
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+//
+//            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_PHONE_STATE}, 113);
+//            return;
+//        } else {
+//            telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+//            IMEI = telephonyManager.getDeviceId();
+//        }
+//
+//    }
 
-    @Override
-    public void onPause() {
-        super.onPause();  // Always call the superclass method first
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_PHONE_STATE}, 113);
-            return;
-        } else {
-            telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-            IMEI = telephonyManager.getDeviceId();
-        }
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 113: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-                    IMEI = telephonyManager.getDeviceId();
-                    return;
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                onResume();
-                return;
-            }
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+//        switch (requestCode) {
+//            case 113: {
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    // permission was granted, yay! Do the
+//                    // contacts-related task you need to do.
+//                    telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+//                    IMEI = telephonyManager.getDeviceId();
+//                    return;
+//
+//                } else {
+//
+//                    // permission denied, boo! Disable the
+//                    // functionality that depends on this permission.
+//                }
+//                onResume();
+//                return;
+//            }
+//            // other 'case' lines to check for other
+//            // permissions this app might request
+//        }
+//    }
 }
